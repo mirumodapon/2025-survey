a9942245619b8380fb99ff265b583e5002ad1689
yoyo930021 <yoyo930021@gmail.com>
2022-07-09 16:56:42 +0800

    Add base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/coscup2022-survey/',
   plugins: [
     vue({
       include: [/\.vue$/]
