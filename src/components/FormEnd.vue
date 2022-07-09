a4ffe43dca81858073cd875d36d986a9fedee66f
yoyo930021 <yoyo930021@gmail.com>
2022-07-09 16:53:31 +0800

    Compress banner image

--- src/components/FormEnd.vue
+++ src/components/FormEnd.vue
@@ -28,7 +28,7 @@ const forget = () => {
   <section class="base">
     <p class="description">{{ t('感謝你的回答，我們將會在 COSCUP 當天公佈統計結果，盡請期待。\n以下是你的參與網址，於手機上點擊並安裝能了解議程與參與當天的現場活動。', 'Thank you for completing our survey! We will announce the result on the day of COSCUP. Please stay tuned!\nHere is the link to install the OPass app on your cell phone. You will get all the information (sessions & events) about COSCUP 2022 through the OPass app.') }}</p>
     <div class="opass">
-      <img :src="`https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=${token}&choe=UTF-8`" />
+      <img :src="`https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=${url}&choe=UTF-8`" />
       <a class="button" :href="url"><BaseButton color="#94E73A">{{ t('打開 OPass APP', 'Open OPass APP') }}</BaseButton></a>
       <BaseText readonly :model-value="url" @click="copy" />
       {{ copyMessage }}
