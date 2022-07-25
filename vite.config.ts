9ffca0e140847c070ce547c6dbd6b8a699f41f0b
yoyo930021 <yoyo930021@gmail.com>
2022-07-25 13:53:26 +0800

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        report: resolve(__dirname, 'report.html')
+        report: resolve(__dirname, 'report/index.html')
       }
     }
   }
